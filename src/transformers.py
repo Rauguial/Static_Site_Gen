e898f2e1d1f8515b672f57a28125b8878fcdd753
Rauko <Nils.Bonefass@outlook.de>
2025-03-29 11:12:56 +0100

    webpage works mostly

--- src/transformers.py
+++ src/transformers.py
@@ -20,7 +20,7 @@ def text_node_to_html_node(text_node):
         case TextType.CODE:
             return LeafNode("code", text_node.text)
         case TextType.IMAGE:
-            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+            return LeafNode("img", " ", {"src": text_node.url, "alt": text_node.text})
         case TextType.LINK:
             if not text_node.url:
                 raise ValueError("Link TextNode must have URL")
